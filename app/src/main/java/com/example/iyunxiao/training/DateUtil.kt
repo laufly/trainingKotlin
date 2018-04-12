850c68fc92b7b96113ac7e5042de76d06002ca7e
dongxiaofei <dongxiaofei@iyunxiao.com>
2018-04-12 15:04:47 +0800

    add a easy test

---
+++ app/src/main/java/com/example/iyunxiao/training/DateUtil.kt
@@ -0,0 +1,15 @@
+package com.example.iyunxiao.training
+
+import java.util.*
+
+/**
+ * Created by dongxiaofei on 2018/4/12.
+ */
+
+object DateUtil{
+
+    fun timestamp2Date(timestamp:Long): Date{
+        return Date(timestamp)
+    }
+
+}
--- app/src/test/java/com/example/iyunxiao/training/ExampleUnitTest.kt
+++ app/src/test/java/com/example/iyunxiao/training/ExampleUnitTest.kt
@@ -3,6 +3,7 @@ package com.example.iyunxiao.training
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.util.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +14,13 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun dateConvertTest(){
+        val timestamp = 1414141414141
+        assertEquals(DateUtil.timestamp2Date(timestamp).toString(), Date(timestamp).toString())
+        assertNotEquals(DateUtil.timestamp2Date(timestamp), Date(System.currentTimeMillis()))
+
     }
 }
